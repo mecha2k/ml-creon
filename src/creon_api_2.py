0f8f489eb28657f03f9860a3945faad8b6bbea92
mecha2k <mecha2k@naver.com>
2021-11-14 09:38:54 +0900

    marketeye

--- src/creon_api_2.py
+++ src/creon_api_2.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import pprint
 import os
 from dotenv import load_dotenv
 from creon import Creon
@@ -14,5 +16,30 @@ if __name__ == "__main__":
     if conn is True:
         print("connection established to creonPlus...")
 
-    data = creon.get_chart("005930", n=60)
-    print(data)
+    codes = creon.get_stockcodes(1)  # kospi=1, kosdaq=2
+    print(codes[:10])
+    print("kospi stock counts: ", len(codes))
+
+    ticker = "005930"
+    index = codes.index("A" + ticker)
+
+    # status = creon.get_stockstatus(codes[index])
+    # print(status)
+
+    features = creon.get_stockfeatures(codes[index])
+    pprint.pp(features)
+
+    # marcap = creon.get_marketcap(target="1")  # '1': KOSPI200, '2': 거래소전체, '4': 코스닥전체
+    # marcap_df = pd.DataFrame(marcap)
+    # marcap_df.set_index("code", inplace=True)
+    # marcap_df.to_pickle("data/marcap.pkl")
+
+    marcap_df = pd.read_pickle("data/marcap.pkl")
+    print(marcap_df.head())
+    print(marcap_df.loc[ticker])
+
+    # balance = creon.get_balance()
+    # print(balance)
+
+    # codes = creon.get_chart("005930", n=60)
+    # print(codes)
