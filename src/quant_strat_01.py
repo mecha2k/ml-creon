eea1c9e98ef718077d4609c4ed9d9129e89f4d99
mecha2k <mecha2k@naver.com>
2021-11-24 15:44:53 +0900

    quant_strat_01.py

--- src/quant_strat_01.py
+++ src/quant_strat_01.py
@@ -65,13 +65,40 @@ def get_marcap_period(start, end):
     return pd.concat(data, keys=keys)
 
 
+def get_dart_fs(codes, year):
+    print(f"{len(codes)} companies in {year} is downloading...")
+
+    fs_df, fs_all_df = list(), list()
+    fs_no, fs_all_no = 0, 0
+    for code in codes:
+        fs = dart.finstate(code, year)
+        fs_all = dart.finstate_all(code, year)
+        if fs is not None:
+            fs_df.append(fs)
+            fs_no += 1
+            print(f"fs_net {code} ({fs_no}th) appended...")
+        if fs_all is not None:
+            fs_all_df.append(fs_all)
+            fs_all_no += 1
+            print(f"fs_all {code} ({fs_all_no}th) appended...")
+
+    fs_df = pd.concat(fs_df, keys=codes)
+    fs_all_df = pd.concat(fs_all_df, keys=codes)
+
+    fs_df.to_pickle(f"data/dart_{year}_df.pkl")
+    fs_all_df.to_pickle(f"data/dart_all_{year}_df.pkl")
+    print(f"fs_net: {fs_no}, fs_all: {fs_all_no} companiess saved in total of {len(codes)} ones")
+
+    return fs_df, fs_all_df
+
+
 if __name__ == "__main__":
     load_dotenv(verbose=True)
     api_key = os.getenv("dart_key")
     dart = OpenDartReader(api_key)
 
-    start = datetime(2000, 1, 1)
-    end = datetime(2021, 12, 31)
+    start = datetime(2015, 1, 1)
+    end = datetime(2016, 12, 31)
 
     try:
         df = pd.read_pickle("data/marcap_period.pkl")
@@ -94,20 +121,43 @@ if __name__ == "__main__":
     marcap_df = pd.concat(marcap_df)
     marcap_df = marcap_df.droplevel(1).rename_axis("Date")
 
+    fs, fs_all, keys = list(), list(), list()
     for dt in pd.date_range(start=start, end=end, freq="AS"):
-        fs = dart.finstate("005380", dt.year)
-        # fs.rename(columns=fs_col_names, inplace=True)
-        # fs = fs.loc[(fs["개별/연결구분"] == "CFS")]
-        # fs = fs[["계정명", "당기금액"]].set_index("계정명")
-        # keys.append(dt)
-        # corp_fs.append(fs)
-        print(dt, fs)
+        data = df.loc[str(dt.year)].copy()
+        codes = data["Code"].values
+        try:
+            fs_df = pd.read_pickle(f"data/dart_{dt.year}_df.pkl")
+            fs_all_df = pd.read_pickle(f"data/dart_all_{dt.year}_df.pkl")
+        except FileNotFoundError:
+            fs_df, fs_all_df = get_dart_fs(codes=codes, year=dt.year)
 
-    # dart = dart_api.init_dart()
-    #
-    # fiscal_df = list()
-    # for dt in pd.date_range(start=start, end=end, freq="AS"):
-    #     fs_df, fs_all_df = list(), list()
-    #     for code in marcap_df.loc[str(dt.year)]["Code"].values:
-    #         fs, fs_all = dart_api.get_corp_fs(dart, code, start, end)
-    #         print(code)
+        fs_df.rename(columns=fs_col_names, inplace=True)
+        # fs_df = fs_df.loc[(fs_df["개별/연결구분"] == "CFS")]
+        fs_df = fs_df[["계정명", "당기금액"]].droplevel(1).reset_index()
+        fs_df = fs_df.rename({"index": "종목"}, axis=1).set_index("종목")
+
+        fs_all_df.rename(columns=fs_col_names, inplace=True)
+        # fs_all_df = fs_all_df.loc[
+        #     (fs_all_df["재무제표구분"] == "BS")
+        #     | (fs_all_df["재무제표구분"] == "CIS")
+        #     | (fs_all_df["재무제표구분"] == "IS")
+        # ]
+        fs_all_df = fs_all_df[["계정명", "당기금액"]].droplevel(1).reset_index()
+        fs_all_df = fs_all_df.rename({"index": "종목"}, axis=1)
+
+        fs.append(fs_df)
+        fs_all.append(fs_all_df)
+        keys.append(dt.year)
+
+    fs_df = pd.concat(fs, keys=keys)
+    fs_all_df = pd.concat(fs_all, keys=keys)
+    fs_df = fs_df.droplevel(1).rename_axis("Time")
+    fs_all_df = fs_all_df.droplevel(1).rename_axis("Time")
+
+    idx = pd.IndexSlice
+    fs_all_df.info()
+    # print(fs_all_df.loc["2015"][fs_all_df["종목"] == "005930"])
+    print(fs_all_df.head())
+    # print(fs_all_df.loc[idx["2015", "005930"], :])
+    fs_df.to_csv("data/fs_df.csv", encoding="utf-8-sig")
+    fs_all_df.to_csv("data/fs_all_df.csv", encoding="utf-8-sig")
