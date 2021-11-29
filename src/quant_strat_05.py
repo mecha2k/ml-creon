249da618bc459878f2279ae961d23b72f4096daf
mecha2k <mecha2k@naver.com>
2021-11-29 21:31:44 +0900

    quant_strat_05.py

--- src/quant_strat_05.py
+++ src/quant_strat_05.py
@@ -207,7 +207,7 @@ def confirm_strategy(start, fdr_df, fs_df):
 if __name__ == "__main__":
     stock_no = 10
     start = datetime(2012, 5, 1)
-    print(f"start : {start}")
+    print(f"start : {start}, stock_no : {stock_no}")
 
     stime = time.time()
     fs_df, creon_df, fdr_df = get_investing_info_data()
